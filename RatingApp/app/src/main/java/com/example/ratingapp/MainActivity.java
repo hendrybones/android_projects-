1ceecef191c2f3e001947d5434e5849d0f2b3bdd
mr bones <bones@localhost.localdomain>
2019-11-22 22:00:43 +0300

    rating app

---
+++ RatingApp/app/src/main/java/com/example/ratingapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ratingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final RatingBar simpleRatingBar=(RatingBar)findViewById(R.id.simpleRatingBar);
+        Button submitButton=(Button)findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String totalStars="Total stars::"+simpleRatingBar.getNumStars();
+                String rating="Rating::"+ simpleRatingBar.getRating();
+                Toast.makeText(getApplicationContext(), totalStars + "\n" + rating, Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+}
